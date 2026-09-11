870bb31e9746c7dd7e7feeb04acf2f5b232fc690
hallankazale <48835559+hallankazale@users.noreply.github.com>
2026-09-11 14:08:26 -0400

    feat: add GIF Maker entrypoint

---
+++ src/gifmaker/main-gifmaker.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GifMakerApp } from "./GifMakerApp";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <GifMakerApp />
+  </React.StrictMode>,
+);
