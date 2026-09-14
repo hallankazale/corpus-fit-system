ebee15ea9472421d0deb4b630af988042792f750
hallankazale <48835559+hallankazale@users.noreply.github.com>
2026-09-13 20:57:01 -0400

    fix(gif-maker): use v1 AI app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { OriginalModeApp } from "./gifmaker/OriginalModeApp";
+import { V1App } from "./gifmaker/V1App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <OriginalModeApp />
+    <V1App />
   </React.StrictMode>,
 );
