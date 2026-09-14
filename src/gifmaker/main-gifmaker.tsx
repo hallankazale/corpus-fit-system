a02b40fe34290debeb492d3c0ee54d20bfed4ddb
hallankazale <48835559+hallankazale@users.noreply.github.com>
2026-09-13 20:05:27 -0400

    feat(gif-maker): switch to v1 production app

--- src/gifmaker/main-gifmaker.tsx
+++ src/gifmaker/main-gifmaker.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { OriginalModeApp } from "./OriginalModeApp";
+import { V1App } from "./V1App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <OriginalModeApp />
+    <V1App />
   </React.StrictMode>,
 );
